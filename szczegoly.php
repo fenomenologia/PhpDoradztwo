a7e89adb0c8ea50f21f90a0cfd8d9807088e9354
Maciej Maślanka <maciek.maslanka@o2.pl>
2025-04-15 18:28:17 +0200

    Utworzenie strony szczegoly.php

---
+++ szczegoly.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<?php
+require_once "conn.php";
+session_start();
+?>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Szczegó³y</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+</body>
+</html>
