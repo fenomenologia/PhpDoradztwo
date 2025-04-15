042d2757696da3ac2f5db3e580c95f82acdbc18b
Maciej Maślanka <maciek.maslanka@o2.pl>
2025-04-15 19:53:21 +0200

    dodano obsługę błędów i powrót do panelu doradcy

--- szczegoly.php
+++ szczegoly.php
@@ -11,48 +11,50 @@ require_once "conn.php";
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
-    <div class="container-fluid">
+    <div class="container-fluid text-center w-50">
         <?php
         $id_klienta = $_POST['id'];
-        $sql = "SELECT doradztwo.data, cecha.nazwa, wynik.punkty
+        $sql = "SELECT klient.imie, klient.nazwisko, doradztwo.data, cecha.nazwa, wynik.punkty
         FROM klient JOIN doradztwo ON doradztwo.id_klienta = klient.id 
         JOIN wynik ON wynik.id_doradztwa = doradztwo.id 
         JOIN cecha ON cecha.id = wynik.id_cechy 
         WHERE klient.id = " . $id_klienta;
         if ($result = mysqli_query($conn, $sql))
         {
+            echo "<p class='h2'>Doradztwa klienta</p>";
             $ilosc_doradztw = mysqli_num_rows($result) / 5;
-            for ($i=0; $i<$ilosc_doradztw; $i++)
+            echo "<table class='table table-bordered'>
+                    <thead>
+                        <tr>
+                            <th>Data doradztwa</th>
+                            <th>Cecha</th>
+                            <th>Ilość zdobytych punktów</th>
+                        </tr>
+                    </thead>
+                    <tbody>";
+            for ($j = 0; $j < $ilosc_doradztw*5; $j++)
             {
-                echo "<table class='table table-bordered'>
-                        <thead>
-                            <tr>
-                              <th>Data doradztwa</th>
-                              <th>Cecha</th>
-                              <th>Ilość zdobytych punktów</th>
-                            </tr>
-                        </thead>
-                        <tbody>";
-                for ($j = 0; $j < 5; $j++)
+                $row = mysqli_fetch_assoc($result);
+                if ($j % 5 ==0)
                 {
-                    $row = mysqli_fetch_assoc($result);
-                    if ($j == 0)
-                    {
-                        echo "<tr><td rowspan=5>" . $row['data'] . "</td><td>" . $row['nazwa'] . "</td><td>" . $row['punkty'] . "</td></tr>";
-                    }
-                    else
-                    {
-                        echo "<tr><td>" . $row['nazwa'] . "</td><td>" . $row['punkty'] . "</td></tr>";
-                    }
+                    echo "<tr><td rowspan=5>" . $row['data'] . "</td><td>" . $row['nazwa'] . "</td><td>" . $row['punkty'] . "</td></tr>";
                 }
-                echo "</tbody></table>";
+                else
+                {
+                    echo "<tr><td>" . $row['nazwa'] . "</td><td>" . $row['punkty'] . "</td></tr>";
+                }
             }
+            echo "</tbody></table>";
         }
         else
         {
-            //błąd bazy
+            echo "<div class='alert alert-danger alert-dismissible'>
+                    <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
+                    <strong>Success!</strong> This alert box could indicate a successful or positive action.
+                </div>";
         }
         ?>
+        <a class="btn btn-primary" href="doradca.php">Powrót do strony głównej</a>
     </div>
 </body>
 </html>
