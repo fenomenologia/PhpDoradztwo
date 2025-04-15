3e365cf560a41ff88ec4d550e414ccff054c553b
Maciej Maślanka <maciek.maslanka@o2.pl>
2025-04-15 19:26:55 +0200

    Naprawa błędów 2

--- szczegoly.php
+++ szczegoly.php
@@ -4,7 +4,7 @@ require_once "conn.php";
 ?>
 <html lang="pl">
 <head>
-    <meta charset="utf-8">
+    <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Szczego³y</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
