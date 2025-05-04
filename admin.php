48bbe1bf4a3d168ded1fbc0fcb8410253af28263
Bartlomien1234567890 <barti.lis@o2.pl>
2025-05-04 13:15:21 +0200

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<?php
+require_once "conn.php";
+session_start();
+?>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Panel administratora</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <div class="container-fluid text-center">
+        <form method="POST">
+            <br><br><br><br><br>
+                <input type="submit" value="Wyœwietl liste doradców" name="wys_doradce" class="btn btn-primary">
+            <br><br>
+                <input type="submit" value="Wyœwietl liste przeprowadzonych doradztw" name="wys_doradztrwo" class="btn btn-primary">
+            <br><br>
+                <input type="submit" value="Wyœwietl pytania" name="wys_pytania" class="btn btn-primary">
+            <br><br>
+                <input type="submit" value="Wyœwietl cechy" name="wys_cechy" class="btn btn-primary">
+            <br><br>
+        </form>
+    </div>
+    <?php
+    if(isset($_POST['wys_doradce'])) {
+        header("location: admin_doradcy.php");
+    }
+    if (isset($_POST['wys_doradztwo'])) {
+        header("location: admin_doradztwa.php");
+    }
+    if (isset($_POST['wys_pytania'])) {
+        header("location: admin_pytania.php");
+    }
+    if (isset($_POST['wys_cechy'])) {
+        header("location: admin_cechy.php");
+    }
+    ?>
+</body>
