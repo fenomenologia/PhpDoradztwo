4d2a13dabd3965224e2ce37e46f2edd841ab5fee
mikolaj-juryk <mikolaj-juryk@wp.pl>
2025-05-05 19:04:40 +0200

    dodany  Wyniki motywacji klienta, Wyniki osobowości klienta i Wyniki stylów uczenia klienta

--- szczegoly.php
+++ szczegoly.php
@@ -74,6 +74,163 @@ session_start();
                     <strong>Błąd!</strong> Wystąpił błąd.
                 </div>";
         }
+        $sql_motyw = "
+        SELECT doradztwo.data,wynik_motywacje.id_pytania, wynik_motywacje.punkty
+        FROM klient
+        JOIN doradztwo ON doradztwo.id_klienta = klient.id
+        JOIN wynik_motywacje ON wynik_motywacje.id_doradztwa = doradztwo.id
+        WHERE klient.id = $id_klienta";
+
+        $result_motyw = mysqli_query($conn, $sql_motyw);
+
+        if ($result_motyw && mysqli_num_rows($result_motyw) > 0) 
+        {
+            echo "<p class='h2 mt-5'>Wyniki motywacji klienta</p>";
+            echo "<table class='table table-bordered'>
+            <thead class='table-primary'>
+                <tr>
+                    <th>Data doradztwa</th>
+                    <th>ID pytania</th>
+                    <th>Punkty</th>
+                </tr>
+            </thead>
+            <tbody>";
+
+            $rows = mysqli_fetch_all($result_motyw, MYSQLI_ASSOC);
+            $total = count($rows);
+
+            for ($i = 0; $i < $total; $i++) 
+            {
+                $row = $rows[$i];
+                if ($i % 5 == 0) 
+                {
+                    echo "<tr>
+                    <td rowspan='5' class='align-middle'>{$row['data']}</td>
+                    <td>{$row['id_pytania']}</td>
+                    <td>{$row['punkty']}</td>
+                  </tr>";
+                } else 
+                {
+                    echo "<tr>
+                    <td>{$row['id_pytania']}</td>
+                    <td>{$row['punkty']}</td>
+                  </tr>";
+                }
+            }
+
+            echo "</tbody></table>";
+        } 
+        else 
+        {
+            echo "<p class='h4 mt-4'>Brak wyników motywacji dla tego klienta.</p>";
+        }
+
+        $sql_osobowosc = "
+        SELECT  doradztwo.data, wynik_osobowosc.mocne_strony, wynik_osobowosc.slabe_strony
+        FROM klient
+        JOIN doradztwo ON doradztwo.id_klienta = klient.id
+        JOIN wynik_osobowosc ON wynik_osobowosc.id_doradztwa = doradztwo.id
+        WHERE klient.id = " . $id_klienta . "";
+
+        $result_osobowosc = mysqli_query($conn, $sql_osobowosc);
+
+        if ($result_osobowosc && mysqli_num_rows($result_osobowosc) > 0) 
+        {
+            echo "<p class='h2 mt-5'>Wyniki osobowości klienta</p>";
+            echo "<table class='table table-bordered'>
+            <thead class='table-primary'>
+                <tr>
+                    <th>Data doradztwa</th>
+                    <th>Mocne strony</th>
+                    <th>Słabe strony</th>
+                </tr>
+            </thead>
+            <tbody>";
+
+            $rows = mysqli_fetch_all($result_osobowosc, MYSQLI_ASSOC);
+            $total = count($rows);
+
+            for ($i = 0; $i < $total; $i++) 
+            {
+                $row = $rows[$i];
+                if ($i % 5 == 0) 
+                {
+                    echo "<tr>
+                    <td class='align-middle'>{$row['data']}</td>
+                    <td>{$row['mocne_strony']}</td>
+                    <td>{$row['slabe_strony']}</td>
+                  </tr>";
+                } else 
+                {
+                    echo "<tr>
+                    <td>{$row['mocne_strony']}</td>
+                    <td>{$row['slabe_strony']}</td>
+                  </tr>";
+                }
+            }
+
+            echo "</tbody></table>";
+        } 
+        else 
+        {
+            echo "<p class='h4 mt-4'>Brak wyników osobowości dla tego klienta.</p>";
+        }
+
+        $sql_style = "
+        SELECT doradztwo.data, style_uczenia.nazwa, style_uczenia.tresc, wynik_style_uczenia.punkty
+        FROM klient
+        JOIN doradztwo ON doradztwo.id_klienta = klient.id
+        JOIN wynik_style_uczenia ON wynik_style_uczenia.id_doradztwa = doradztwo.id
+        JOIN style_uczenia ON style_uczenia.id = wynik_style_uczenia.id_stylu
+        WHERE klient.id = " . $id_klienta . "";
+
+        $result_style = mysqli_query($conn, $sql_style);
+
+        if ($result_style && mysqli_num_rows($result_style) > 0) 
+        {
+            echo "<p class='h2 mt-5'>Wyniki stylów uczenia klienta</p>";
+            echo "<table class='table table-bordered'>
+            <thead class='table-primary'>
+                <tr>
+                    <th>Data doradztwa</th>
+                    <th>Nazwa stylu</th>
+                    <th>Treść stylu</th>
+                    <th>Punkty</th>
+                </tr>
+            </thead>
+            <tbody>";
+
+            $rows = mysqli_fetch_all($result_style, MYSQLI_ASSOC);
+            $total = count($rows);
+
+            for ($i = 0; $i < $total; $i++) 
+            {
+                $row = $rows[$i];
+                if ($i % 5 == 0) 
+                {
+                    echo "<tr>
+                    <td rowspan='3' class='align-middle'>{$row['data']}</td>
+                    <td>{$row['nazwa']}</td>
+                    <td>{$row['tresc']}</td>
+                    <td>{$row['punkty']}</td>
+                  </tr>";
+                } else 
+                {
+                    echo "<tr>
+                    <td>{$row['nazwa']}</td>
+                    <td>{$row['tresc']}</td>
+                    <td>{$row['punkty']}</td>
+                  </tr>";
+                }
+            }
+
+            echo "</tbody></table>";
+        } 
+        else 
+        {
+            echo "<p class='h4 mt-4'>Brak wyników stylów uczenia dla tego klienta.</p>";
+        }
+
         if (isset($_POST['usun']))
         {
             $sql = 'SELECT doradztwo.id FROM doradztwo WHERE id_klienta =' . $id_klienta;
