116fcb9ad04eabb5d48a2591af9d0ebc38fd4161
Maciej Maślanka <maciek.maslanka@o2.pl>
2025-05-15 18:23:45 +0200

    dodano błędy przy logowaniu

--- index.php
+++ index.php
@@ -41,6 +41,62 @@
 		</div>
 	  </div>
 	</nav>
+	<?php
+	if (isset($_POST['zaloguj']))
+	{
+		$login = $_POST['login'];
+		$pass = $_POST['pass'];
+		$sql1 = "SELECT id, email, haslo FROM klient WHERE email = '$login' AND status = 1";
+		$sql2 = "SELECT id, email, haslo, czy_admin FROM doradca WHERE email = '$login' AND czy_aktywny = 1";
+		$resultKlient = mysqli_query($conn, $sql1);
+		$resultDoradca = mysqli_query($conn, $sql2);
+
+		if (mysqli_num_rows($resultKlient) != 0) //jeżeli email klienta jest w bazie
+		{
+			$row = mysqli_fetch_array($resultKlient);
+			if (password_verify($pass, $row['haslo'])) //jeżeli hasło jest poprawne
+			{
+				//przejdź do strony z kwestionariuszem i zapisz id klienta
+				session_start();
+				$_SESSION['id_klienta'] = $row['id'];
+				header("Location: main_site.php");
+			}
+			else //jeżeli hasło jest złe
+			{
+				$msg = "PODANO NIEPOPRAWNE HASŁO!";
+			}
+		}
+		else if (mysqli_num_rows($resultDoradca) != 0) // jeżeli email doradcy jest w bazie
+		{
+			$row = mysqli_fetch_array($resultDoradca);
+			if (password_verify($pass, $row['haslo'])) //jeżeli hasło jest poprawne
+			{
+				if ($row['czy_admin'] == 0)
+				{
+					//przejdź do strony doradcy i zapisz id doradcy
+					session_start();
+					$_SESSION['id_doradcy'] = $row['id'];
+					header("Location: doradca.php");
+				}
+				else
+				{
+					//przejdź do strony admina i zapisz jego id
+					session_start();
+					$_SESSION['id_admina'] = $row['id'];
+					header("Location: admin.php");
+				}
+			}
+			else //jeżeli hasło jest złe
+			{
+				$msg = "PODANO NIEPOPRAWNE HASŁO!";
+			}
+		}
+		else //jeżeli emaila nie ma w bazie
+		{
+			$msg = "PODANO NIEPOPRAWNY LOGIN!";
+		}
+	}
+	?>
 	<main class="container-fluid text-white bg-image bg-primary mt-5 flex-fill">
 		<div class="container-fluid text-center w-25">
 			<p class="h2 fw-bold">ZALOGUJ SIĘ</p>
@@ -53,64 +109,10 @@
 					<input type="password" name="pass" id="pass" class="form-control" required placeholder="Podaj hasło">
 					<label for="pass" class="form-label">Hasło</label>
 				</div>
-				<input type="submit" value="ZALOGUJ SIĘ" name="zaloguj" class="btn btn-primary fw-bold">
+				<span style="color: red" class="fw-bold mb-3 mt-3"><?php if (isset($msg))
+					echo $msg;?></span><br />
+				<input type="submit" value="ZALOGUJ SIĘ" name="zaloguj" class="btn btn-primary fw-bold mt-3">
 			</form>
-			<?php
-			if (isset($_POST['zaloguj']))
-			{
-				$login = $_POST['login'];
-				$pass = $_POST['pass'];
-				$sql1 = "SELECT id, email, haslo FROM klient WHERE email = '$login' AND status = 1";
-				$sql2 = "SELECT id, email, haslo, czy_admin FROM doradca WHERE email = '$login' AND czy_aktywny = 1";
-				$resultKlient = mysqli_query($conn, $sql1);
-				$resultDoradca = mysqli_query($conn, $sql2);
-
-				if (mysqli_num_rows($resultKlient) != 0) //jeżeli email klienta jest w bazie
-				{
-					$row = mysqli_fetch_array($resultKlient);
-					if (password_verify($pass, $row['haslo'])) //jeżeli hasło jest poprawne
-					{
-						//przejdź do strony z kwestionariuszem i zapisz id klienta
-						session_start();
-						$_SESSION['id_klienta'] = $row['id'];
-						header("Location: main_site.php");
-					}
-					else //jeżeli hasło jest złe
-					{
-						//złe hasło
-					}
-				}
-				else if (mysqli_num_rows($resultDoradca) != 0) // jeżeli email doradcy jest w bazie
-				{
-					$row = mysqli_fetch_array($resultDoradca);
-					if (password_verify($pass, $row['haslo'])) //jeżeli hasło jest poprawne
-					{
-						if ($row['czy_admin'] == 0)
-						{
-							//przejdź do strony doradcy i zapisz id doradcy
-							session_start();
-							$_SESSION['id_doradcy'] = $row['id'];
-							header("Location: doradca.php");
-						}
-						else
-						{
-							//przejdź do strony admina i zapisz jego id
-							session_start();
-							$_SESSION['id_admina'] = $row['id'];
-							header("Location: admin.php");
-						}
-					}
-					else //jeżeli hasło jest złe
-					{
-						//złe hasło
-					}
-				}
-				else //jeżeli emaila nie ma w bazie
-				{
-					//zły login
-				}
-			}
-			?>
 		</div>
 	</main>
 	<?php
